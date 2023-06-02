84917be8a00642ac7941b17c4341c75f97c31950
Libovski <goncharov199306@gmail.com>
2023-06-02 20:32:05 +0700

    Initial commit

---
+++ src/CustomTree/CustomTree.java
@@ -0,0 +1,17 @@
+package CustomTree;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/CustomTree/Solution.java
@@ -0,0 +1,9 @@
+package CustomTree;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+    }
+}
