6a91e0b2ad0b6c38229f239ab07e84dfa230d4b2
Libovski <goncharov199306@gmail.com>
2023-06-02 22:36:49 +0700

    Add inner class Entry<T>

--- src/CustomTree/CustomTree.java
+++ src/CustomTree/CustomTree.java
@@ -46,5 +46,21 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     public int size() {
         return 0;
     }
+    static class  Entry<T> implements Serializable{
+        String elementName;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+        Entry<T> parent;
+        Entry<T> leftChild;
+        Entry<T> rightChild;
 
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            this.availableToAddLeftChildren = true;
+            this.availableToAddRightChildren = true;
+        }
+        public boolean isAvailableToAddChildren(){
+            return availableToAddLeftChildren | availableToAddRightChildren;
+        }
+    }
 }
