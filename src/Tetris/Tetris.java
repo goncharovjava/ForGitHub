42ee2dd12713bbd49af85ebbf163c6dfc1c39ae4
Libovski <goncharov199306@gmail.com>
2023-06-22 20:01:09 +0700

    add two getters in Tetris

--- src/Tetris/Tetris.java
+++ src/Tetris/Tetris.java
@@ -3,6 +3,15 @@ package Tetris;
 public class Tetris {
     private Field field;
     private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args){
 
     }
